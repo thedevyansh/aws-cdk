0f8c8972e773c79815e248dfba0245dd0919363f
thedevyansh <schawla333333@gmail.com>
2022-02-11 15:44:23 +0530

    Initial commit

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class TypescriptCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +8,8 @@ export class TypescriptCdkStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TypescriptCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, 'DocumentsBucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
   }
 }
