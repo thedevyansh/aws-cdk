bd24e186b6c0da040d5feee71e533b7b1b68499a
thedevyansh <schawla333333@gmail.com>
2022-02-12 13:12:15 +0530

    Add construct for configuring VPC and tags

---
+++ lib/networking.ts
@@ -0,0 +1,33 @@
+import { Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+interface NetworkingProps {
+    maxAzs: number
+}
+
+export class Networking extends Construct {
+    public readonly vpc: ec2.IVpc
+
+    constructor(scope: Construct, id: string, props: NetworkingProps) {
+        super(scope, id);
+
+        this.vpc = new ec2.Vpc(this, 'AppVPC', {
+            cidr: '10.0.0.0/16',
+            maxAzs: props.maxAzs,
+            subnetConfiguration: [
+                {
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    name: 'Public',
+                    cidrMask: 24,
+                },
+                {
+                    cidrMask: 24,
+                    name: 'Private',
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+                },
+
+            ],
+        });
+    }
+}
--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Networking } from './networking'
 
 export class TypescriptCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,5 +12,16 @@ export class TypescriptCdkStack extends Stack {
     const bucket = new s3.Bucket(this, 'DocumentsBucket', {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
+
+    new CfnOutput(this, 'DocumentsBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'DocumentsBucketName'
+    })
+
+    const networkingConstruct = new Networking(this, 'NetworkingConstruct', {
+      maxAzs: 2
+    })
+
+    Tags.of(networkingConstruct).add('Module', 'Networking')
   }
 }
